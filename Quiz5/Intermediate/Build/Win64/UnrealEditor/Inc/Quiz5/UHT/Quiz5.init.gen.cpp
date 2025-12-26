956e8c07bf9ad059b506606745c5fd0379b6b37d
NMEii <82252932+NMEii@users.noreply.github.com>
2025-12-26 19:59:06 +0900

    push

---
+++ Quiz5/Intermediate/Build/Win64/UnrealEditor/Inc/Quiz5/UHT/Quiz5.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeQuiz5_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Quiz5;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Quiz5()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Quiz5.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Quiz5",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x1C13BAE9,
+				0x607BFA29,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Quiz5.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Quiz5.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Quiz5(Z_Construct_UPackage__Script_Quiz5, TEXT("/Script/Quiz5"), Z_Registration_Info_UPackage__Script_Quiz5, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x1C13BAE9, 0x607BFA29));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Quiz5/Source/Quiz5/Private/MyActor.cpp
@@ -0,0 +1,103 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+#include  "Engine/Engine.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	turnCount = 0;
+	moveCount = 0;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	SetActorLocation(FVector(0, 50, 0));
+	startLocation = GetActorLocation();
+	
+	for (int i = 0; i < 10; i++)
+	{
+		RandomEvent();
+	}
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			10.0f,
+			FColor::Blue,
+			FString::Printf(TEXT("Turn Count : %d"), turnCount)
+		);
+
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			10.0f,
+			FColor::Blue,
+			FString::Printf(TEXT("Move Count : %d"), moveCount)
+		);
+
+	}
+
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMyActor::Turn()
+{
+	SetActorRotation(FRotator(100.f, 100.f, 100.f));
+	actorLocation = GetActorLocation();
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			10.0f,
+			FColor::Yellow,
+			*actorLocation.ToString()
+		);
+	}
+
+}
+
+void AMyActor::Move()
+{
+	AddActorLocalOffset(FVector(50.f, 0.f, 0.f));
+	actorLocation = GetActorLocation();
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			10.0f,
+			FColor::Green,
+			*actorLocation.ToString()
+		);
+	}
+}
+
+void AMyActor::RandomEvent()
+{
+	int i_random = FMath::RandRange(0, 100);
+
+	if (i_random > 50)
+	{
+		moveCount++;
+		Move();
+	}
+	else
+	{
+		turnCount++;
+		Turn();
+	}
+}
+
